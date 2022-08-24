96828d593adf6e1537cdf5b77dd97894ae78ba44
Kurnia <92375799+KurniaRamadhani11@users.noreply.github.com>
2022-08-24 12:12:42 +0700

    Add files via upload
    backup6

---
+++ kurniamavenstu2/src/main/java/com/mycompany/kurniamavenstu2/Kurniamavenstu2.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.kurniamavenstu2;
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author ASUS
+ */
+public class Kurniamavenstu2 {
+
+    public static void main(String[] args) {
+       //CARA DEKLARASI
+        //int x=10;
+        //double y=1.5;
+        String a="KURNIA LAGI APA?";
+       String b=" KURNIA LAGI BELAJAR";
+       System.out.println(a + b);
+       // System.out.println(x);
+       // System.out.println(y);
+       
+       //input
+       // Scanner tes = new Scanner(System.in);
+        //String n = tes.next();
+        //String p = tes.next();
+        //String h = tes.next();
+       // int t = tes.nextInt();
+        
+        //switch(t){
+       // case 2 -> System.out.println("dua");
+       //case 3 -> System.out.println("tiga");
+       //default -> System.out.println("gada");
+        //}
+                
+        //System.out.println("NAMA\t: "+n);
+        //System.out.println("NPM\t: "+p);
+        //System.out.println("HOBI\t: "+h);
+        
+        //PERCABANGAN
+       // if(t==3){
+            //System.out.println("tiga");
+       // }
+       // else{
+           // System.out.println("bukan tigaa woi");
+       // }
+      
+       //LOOPING
+//       for (int i = 1; i < 4; i++) {
+//           Scanner tes = new Scanner(System.in);
+//            int n = tes.nextInt();
+//           System.out.println(n);
+// // System.out.println(i);
+//}
+      
+       
+       }
+}
+
