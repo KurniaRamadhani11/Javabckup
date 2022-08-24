5433fee5e2164f306e254d65e43e2aa053d15783
Kurnia <92375799+KurniaRamadhani11@users.noreply.github.com>
2022-08-24 12:06:54 +0700

    Add files via upload
    backup4

---
+++ Kurniamavenstu4/src/main/java/com/mycompany/kurniamavenstu4/Kurniamavenstu4.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.kurniamavenstu4;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Kurniamavenstu4 {
+
+    public static void main(String[] args) {
+         ArrayList<Integer> arr = new ArrayList();
+         arr.add(1);
+         arr.add(2);
+         arr.add(3);
+         print(arr);
+    }
+    public static void print(ArrayList<Integer>arr){
+        for(int i=0; i<arr.size(); i++){
+            System.out.println(arr.get(i));
+        }
+            
+    }
+    
+    }
+     //Scanner input = new Scanner(System.in);
+//         int a=input.nextInt();
+//         int b=input.nextInt();
+//       // tambah(2,3);
+//        //System.out.println("Hello World!");
+//        jumlah(a,b);
+//   }
+   // public static void tambah(int a, int b){
+       // System.out.println("Nilai a :"+a+"\nNilai b : "+b);
+        //System.out.println(a+b);
+    //}
+//     public static int jumlah(int a, int b){
+//         int k = a+b;
+//         System.out.println("Nilai a :"+a+"\nNilai b : "+b);
+//         System.out.println(k);
+//         return k;
+         
+         
+     
+
